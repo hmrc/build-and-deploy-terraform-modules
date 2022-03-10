c454c772e4f8530869f15ebb9ce5039be6727acb
waddamski <andy.waddams@digital.hmrc.gov.uk>
2022-03-10 16:06:17 +0000

    PBD-3419 - moved codebuild terraform module to this repo.
    Co-authored-by: Neil Millard <3050649+neilmillard@users.noreply.github.com>

--- codebuild/pipeline/iam.tf
+++ codebuild/pipeline/iam.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role" "codebuild_execution" {
+resource "aws_iam_role" "codebuild_execution_role" {
   name = var.codebuild_pipeline_name
   assume_role_policy = <<EOF
 {
@@ -16,23 +16,61 @@ resource "aws_iam_role" "codebuild_execution" {
 EOF
 }
 
-resource "aws_iam_role_policy" "logging" {
-  name   = "logging"
-  role   = aws_iam_role.codebuild_execution.id
-  policy = jsonencode(
-    {
-      Version = "2012-10-17"
-      Statement = [
-        {
-          Action = [
-            "logs:CreateLogStream",
-            "logs:PutLogEvents",
-            "ssm:GetParameters" 
-          ]
-          Effect   = "Allow"
-          Resource = "${aws_cloudwatch_log_group.main.arn}:*"
-        },
+resource "aws_iam_role_policy" "codebuild_execution_policy" {
+  name   = var.codebuild_pipeline_name
+  role   = aws_iam_role.codebuild_execution_role.id
+  policy = data.aws_iam_policy_document.codebuild_execution_policy_document.json
+}
+
+
+data "aws_iam_policy_document" "codebuild_execution_policy_document" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = [
+      "${aws_cloudwatch_log_group.main.arn}:*",
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ec2:CreateNetworkInterface",
+      "ec2:DescribeSecurityGroups",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeVpcs",
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:DeleteNetworkInterface",
+      "ec2:DescribeDhcpOptions",
+    ]
+
+    resources = [
+      "*",
+    ] 
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ec2:CreateNetworkInterfacePermission"
+    ]
+
+    resources = [
+      "arn:aws:ec2:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:network-interface/*",
+    ]
+    condition {
+      test     = "StringEquals"
+      variable = "ec2:Subnet"
+
+      values = [
+        "arn:aws:ec2:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:subnet/${var.codebuild_subnet_ids[0]}",
+        "arn:aws:ec2:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:subnet/${var.codebuild_subnet_ids[1]}",
+        "arn:aws:ec2:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:subnet/${var.codebuild_subnet_ids[2]}"
       ]
     }
-  )
+  }
 }
--- codebuild/pipeline/outputs.tf
+++ codebuild/pipeline/outputs.tf
@@ -1,3 +1,3 @@
 output "codebuild_execution_role_id" {
-  value = aws_iam_role.codebuild_execution.id
+  value = aws_iam_role.codebuild_execution_role.id
 }
